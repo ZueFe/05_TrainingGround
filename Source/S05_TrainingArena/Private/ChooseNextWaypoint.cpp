95916ef3b8d31179b7e48f67900b5a4b3e05651c
ZueFe <ex3me@centrum.sk>
2018-01-06 22:55:27 +0100

    moved all patrolling behavior to c++

--- Source/S05_TrainingArena/Private/ChooseNextWaypoint.cpp
+++ Source/S05_TrainingArena/Private/ChooseNextWaypoint.cpp
@@ -2,13 +2,26 @@
 
 #include "ChooseNextWaypoint.h"
 #include "BehaviorTree/BlackboardComponent.h"
+#include "PatrollingGuard.h"
+#include "AIController.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
+
+	//TODO: protect against empty patrol route
 
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
 
-	UE_LOG(LogTemp, Warning, TEXT("Waypoint index: %i"), Index);
+	//Get the patrol points
+	auto Controller = Cast<APatrollingGuard>(OwnerComp.GetAIOwner()->GetPawn());
+	auto WaypointsCPP = Controller->WaypointsCPP;
+
+	//Set waypoint
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, WaypointsCPP[Index]);
+
+	//Cycle index
+	Index = (Index + 1) % WaypointsCPP.Num();
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, Index);
 
 	return EBTNodeResult::Succeeded;
 }

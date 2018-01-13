407433092a0d8e1cc580cab4049dfb3e6c97b334
ZueFe <ex3me@centrum.sk>
2018-01-13 23:23:37 +0100

    up to the point where guard holds gun

--- Source/S05_TrainingArena/S05_TrainingArenaGameMode.cpp
+++ Source/S05_TrainingArena/S05_TrainingArenaGameMode.cpp
@@ -2,14 +2,14 @@
 
 #include "S05_TrainingArenaGameMode.h"
 #include "S05_TrainingArenaHUD.h"
-#include "S05_TrainingArenaCharacter.h"
+#include "Player/FirstPersonCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 AS05_TrainingArenaGameMode::AS05_TrainingArenaGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Blueprints/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
